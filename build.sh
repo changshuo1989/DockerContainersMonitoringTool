fe73bd49c9f2ae70acd490d8ead719c28286d1af
Changshuo Gao <cgao@healthrecoverysolutions.com>
2016-02-02 14:50:31 -0500

    install docker-compose

--- build.sh
+++ build.sh
@@ -5,11 +5,20 @@ if [ ! "`whoami`" = "root" ]; then
 	echo "Error: You must be root to run this script!";
 	exit 1;
 fi
-
 # Create self-signed key and certs
 DIR=${PWD}
 cd ${DIR}/postfix/certs/
 ./setup.sh
 cd ${DIR}
+
+#check if docker-compose is installed or not
+COMPOSE="`which docker-compose`"
+if [ -z ${COMPOSE} ]; then
+	PIP="`which pip`"
+        if [ -z ${PIP} ]; then
+        	apt-get -y install python-pip
+        fi
+        pip install docker-compose
+fi
 #Run Prometheus Monitoring system 
 docker-compose up -d
